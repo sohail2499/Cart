03ee4f3f6ff86df3eb2a80e1c238015e290a1347
sohailkhan24 <48204572+sohailkhan24@users.noreply.github.com>
2020-06-30 17:58:51 +0530

    props

--- src/CartItem.js
+++ src/CartItem.js
@@ -3,17 +3,7 @@ import React from 'react';
 class CartItem extends React.Component {
     //need to call the constructor of the parent class if inheriting
   
-    constructor (){
-        super();
-        this.state ={
-            price: 999,
-            title:'Phone',
-            qty:1,
-            img: ''
-        }
-        // this.increaseQuantity = this.increaseQuantity.bind(this);
-        // this.testing();
-    }
+    
 //     testing () {
 //     const promise = new Promise((resolve, reject) => {
 //       setTimeout(() => {
@@ -69,7 +59,8 @@ class CartItem extends React.Component {
 
     }
     render(){
-        const {price,title,qty}=this.state;
+        console.log('this.props',this.props);
+        const {price,title,qty}=this.props.product;
         return(
              <div className="cart-item">
                 <div className= "left-block">
